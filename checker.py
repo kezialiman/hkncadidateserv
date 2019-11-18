8ce30b40b97f9b6f0b92d3c7d925ac077eaba26c
Kezia Liman <54640440+kezialiman@users.noreply.github.com>
2019-11-17 20:56:13 -0800

    Add files via upload

---
+++ checker.py
@@ -0,0 +1,9 @@
+def checker(lst):
+    if(lst[0] != 5):
+        print("A: Not quite. Try again")
+    if(lst[1] != 1):
+        print("B: Not quite. Try again")
+    if(lst[2] != 2):
+    	print("C: Not quite. Try again")
+    if (lst[0] == 5 and lst[1] == 1 and lst[2] == 2):
+    	print("YOU DID IT!")
---
+++ inverse.py
@@ -0,0 +1,10 @@
+def inverse(lst):
+    if(lst[0] != 1):
+        print("A: Not quite. Try again")
+    if(lst[1] != 2):
+        print("B: Not quite. Try again")
+    if(lst[2] != 2):
+        print("C: Not quite. Try again") 
+    if (lst[0] == 1 and lst[1] == 2 and lst[2] == 2):
+    	print("YOU DID IT!")
+        
